9d07916b6dfc6132d035a09d2f5e847b56b15a5a
perez1187 <o.perez1187@gmail.com>
2022-10-23 13:57:55 +0200

    bacground image

--- jakacoachingMVP/src/components/RangeViewerComponentRanges/RangeViewerComponentRanges.jsx
+++ jakacoachingMVP/src/components/RangeViewerComponentRanges/RangeViewerComponentRanges.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import Range1 from '../../assets/range 1.png'
+
+// css
+import './RangeViewerComponentRanges.css'
 
 function RangeViewerComponentRanges() {
   return (
-    <div>RangeViewerComponentRanges</div>
+    <div className='RangeViewerComponentRanges'>
+      <div >
+        <div> Title Range 1</div>
+        <div className='RangeViewerComponentRangesRange'> 
+          <img src={Range1} alt="Logo" />
+        </div>
+      </div>  
+      <div> 
+        <div> Title Range 2</div>
+        <div className='RangeViewerComponentRangesRange'> range</div>
+      </div>
+    </div>
   )
 }
 
