c639825785f7059dc13d3d8c7ad4a9afe3cf96b4
perez1187 <o.perez1187@gmail.com>
2022-10-24 20:20:36 +0200

    adding first stats

--- jakacoachingMVP/src/components/PostflopViewerComponent/PostflopViewerComponent.jsx
+++ jakacoachingMVP/src/components/PostflopViewerComponent/PostflopViewerComponent.jsx
@@ -1,6 +1,10 @@
 import React, {useState, useEffect} from 'react'
 
+// manual data
 import jsonData from '../../assets/example.json'
+
+// css
+import './PostflopViewerComponent.css'
 
 function PostflopViewerComponent() {
 
@@ -81,6 +85,62 @@ function PostflopViewerComponent() {
                 } else {
                     setBet17EV(object["BET 17% freq"])
                 }
+
+                // BET 30% EV
+                if (object["BET 30% EV"] == null) {
+                    setBet30EV('0')
+                } else {
+                    setBet30EV(object["BET 30% EV"])
+                }
+
+                // BET 30% FREQ
+                if (object["BET 30% freq"] == null) {
+                    setBet30Freq('0')
+                } else {
+                    setBet30Freq(object["BET 30% freq"])
+                } 
+
+                // BET 60% EV
+                if (object["BET 60% EV"] == null) {
+                    setBet60EV('0')
+                } else {
+                    setBet60EV(object["BET 60% EV"])
+                }
+                
+                // BET 60% FREQ
+                if (object["BET 60% freq"] == null) {
+                    setBet60Freq('0')
+                } else {
+                    setBet60Freq(object["BET 60% freq"])
+                }
+                
+                // BET 100% EV
+                if (object["BET 100% EV"] == null) {
+                    setBet100EV('0')
+                } else {
+                    setBet100EV(object["BET 100% EV"])
+                }
+                
+                // BET 100% FREQ
+                if (object["BET 100% freq"] == null) {
+                    setBet100Freq('0')
+                } else {
+                    setBet100Freq(object["BET 100% freq"])
+                }
+                
+                // BET check ev
+                if (object["CHECK EV"] == null) {
+                    setBetCHECKEV('0')
+                } else {
+                    setBetCHECKEV(object["CHECK EV"])
+                }
+                
+                // BET check freq
+                if (object["CHECK freq"] == null) {
+                    setBetCHECKFreq('0')
+                } else {
+                    setBetCHECKFreq(object["CHECK freq"])
+                }                                    
                 
             } catch(e) {
                 console.log('cos nie tak')
@@ -96,12 +156,30 @@ function PostflopViewerComponent() {
   return (
     <div> 
         <div onClick={()=> CheckData()}>PostflopViewerComponent</div>
-        <div> Info about the hand: {oneHand}</div>
-        <div>
-            <div>
+        <div>Situation: </div>
+        <div> Info about the hand:</div> 
+        <div className='hand'>{oneHand}</div> 
+        <div className='freqOfHand'>
+            <div className='freqOfHandMiniBox'>
                 <div>17% CB</div>
                 <div>{bet17Freq}</div>
             </div>
+            <div className='freqOfHandMiniBox'>
+                <div>30% CB</div>
+                <div>{bet30Freq}</div>
+            </div>
+            <div className='freqOfHandMiniBox'> 
+                <div>60% CB</div>
+                <div>{bet60Freq}</div>
+            </div>
+            <div className='freqOfHandMiniBox'>
+                <div>100% CB</div>
+                <div>{bet100Freq}</div>
+            </div>
+            <div className='freqOfHandMiniBox'>
+                <div>CHECK freq</div>
+                <div>{betCHECKFreq}</div>
+            </div>                
         </div>
 
         <div>{RenderHands()}</div>
